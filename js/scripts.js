16472cade3e23a10e8dac9e995cc8e172bbe2971
Rydavim <rydavim@gmail.com>
2022-05-12 15:05:31 -0400

    first attempt at generating blog posts

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,41 @@ function cartAdd(fees) {
   alert("Cart Total: $" + cartTotal); // how to format with hundredths?
   return cartTotal;
 }
+
+// blog page article generation
+let blogPosts = [
+  {
+    image: 'images/blog-1.jpg',
+    altText: 'A young man in a tan shirt and his medium-sized black dog sit overlooking a canyon.',
+    heading3: 'Traveling With Your Dog',
+    paragraph1: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.', paragraph2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.'
+  }
+];
+
+const blog = document.getElementById('blog');
+
+for (let i = 0; i < blogPosts.length; i++) {
+  const blogPost = blogPosts[i];
+
+  const storyDiv = document.createElement('div');
+  storyDiv.setAttribute('class', 'story clearfix');
+
+  const postImg = document.createElement('img');
+  postImg.setAttribute('src', blogPost.image);
+  postImg.setAttribute('alt', blogPost.altText);
+  storyDiv.appendChild(postImg);
+
+  const postHeading = document.createElement('h3');
+  postHeading.textContent = blogPost.heading3;
+  storyDiv.appendChild(postHeading);
+
+  const postP1 = document.createElement('p');
+  postP1.textContent = blogPost.paragraph1;
+  storyDiv.appendChild(postP1);
+
+  const postP2 = document.createElement('p');
+  postP2.textContent = blogPost.paragraph2;
+  storyDiv.appendChild(postP2);
+
+  blog.appendChild(storyDiv);
+}
